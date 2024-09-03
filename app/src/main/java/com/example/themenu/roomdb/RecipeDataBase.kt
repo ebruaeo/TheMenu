8015b55dc299f95666bd9461ef6f653fad15e672
ebruaeo <ebruaeo@hotmail.com>
2024-09-03 20:39:57 +0300

    version changed

--- app/src/main/java/com/example/themenu/roomdb/RecipeDataBase.kt
+++ app/src/main/java/com/example/themenu/roomdb/RecipeDataBase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.themenu.model.Recipe
 
-@Database(entities = [Recipe::class], version = 1)
+@Database(entities = [Recipe::class], version = 2)
 abstract class RecipeDataBase : RoomDatabase() {
     abstract fun recipeDao(): RecipeDAO
 }
