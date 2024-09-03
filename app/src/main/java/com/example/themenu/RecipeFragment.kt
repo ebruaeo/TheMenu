bffc9a0f7c08184da161384633efc21fb36a9ed5
ebruaeo <ebruaeo@hotmail.com>
2024-09-03 11:28:10 +0300

    binding and setOnClickListeners for buttons

--- app/src/main/java/com/example/themenu/RecipeFragment.kt
+++ app/src/main/java/com/example/themenu/RecipeFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.themenu.databinding.FragmentListBinding
+import com.example.themenu.databinding.FragmentRecipeBinding
 
 
 class RecipeFragment : Fragment() {
 
-
+    private var _binding: FragmentRecipeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,12 +22,47 @@ class RecipeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+        _binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.saveButton.setOnClickListener { save(it) }
+        binding.deleteButton.setOnClickListener { delete(it) }
+        binding.imageView.setOnClickListener { choosePic(it) }
+
+        arguments?.let {
+            val info = RecipeFragmentArgs.fromBundle(it).info
+
+            if (info == "new"){
+                // yeni bilgi eklenecek
+                binding.deleteButton.isEnabled = false
+                binding.saveButton.isEnabled = true
+            } else {
+                binding.deleteButton.isEnabled = true
+                binding.saveButton.isEnabled = false
+            }
+        }
+    }
+
+    fun save(view: View) {
+
+    }
+
+    fun delete(view: View) {
+
+    }
+
+    fun choosePic(view: View) {
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
