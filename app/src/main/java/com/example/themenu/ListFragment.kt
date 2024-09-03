a09f4875dd274059da0d64717ffc137d06500b6b
ebruaeo <ebruaeo@hotmail.com>
2024-09-03 11:27:26 +0300

    binding and adding setOnClickListener for floatingActionButton

--- app/src/main/java/com/example/themenu/ListFragment.kt
+++ app/src/main/java/com/example/themenu/ListFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.themenu.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +24,24 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.floatingActionButton.setOnClickListener { addNew(it) }
+    }
+
+    fun addNew(view: View){
+        val action = ListFragmentDirections.actionListFragmentToRecipeFragment(info = "new", id = 0)
+        Navigation.findNavController(view).navigate(action)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
