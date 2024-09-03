a573b550155afd78f3fb9f515f5b9c021d48cf7b
ebruaeo <ebruaeo@hotmail.com>
2024-09-03 12:13:26 +0300

    asking permissions from user codes

--- app/src/main/java/com/example/themenu/RecipeFragment.kt
+++ app/src/main/java/com/example/themenu/RecipeFragment.kt
@@ -1,18 +1,42 @@
 package com.example.themenu
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.themenu.databinding.FragmentListBinding
 import com.example.themenu.databinding.FragmentRecipeBinding
+import com.google.android.material.snackbar.Snackbar
+import java.io.IOException
+import java.lang.Exception
 
 
 class RecipeFragment : Fragment() {
 
     private var _binding: FragmentRecipeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private var secilenGorsel: Uri? = null
+    private var secilenBitmap: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,7 +60,7 @@ class RecipeFragment : Fragment() {
         arguments?.let {
             val info = RecipeFragmentArgs.fromBundle(it).info
 
-            if (info == "new"){
+            if (info == "new") {
                 // yeni bilgi eklenecek
                 binding.deleteButton.isEnabled = false
                 binding.saveButton.isEnabled = true
@@ -45,6 +69,8 @@ class RecipeFragment : Fragment() {
                 binding.saveButton.isEnabled = false
             }
         }
+
+        registerLauncher()
     }
 
     fun save(view: View) {
@@ -57,7 +83,117 @@ class RecipeFragment : Fragment() {
 
     fun choosePic(view: View) {
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {// izin verilmemiş
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
+                    // snack bar göstermemiz lazım, kullanıcıdan neden izin istediğimizi açıklamak için
+                    Snackbar.make(view, "Galeriden görsel seçmeliyiz", Snackbar.LENGTH_INDEFINITE)
+                        .setAction(
+                            "izin ver",
+                            View.OnClickListener {
+                                // izin isteyeceğiz
+                                permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
 
+                            }
+                        ).show()
+                } else {
+                    // izin iste
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+
+                }
+            } else {
+                // izin verilmiş galeriye git
+                val intentToGaleri =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGaleri)
+            }
+
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {// izin verilmemiş
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    // snack bar göstermemiz lazım, kullanıcıdan neden izin istediğimizi açıklamak için
+                    Snackbar.make(view, "Galeriden görsel seçmeliyiz", Snackbar.LENGTH_INDEFINITE)
+                        .setAction(
+                            "izin ver",
+                            View.OnClickListener {
+                                // izin isteyeceğiz
+                                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                            }
+                        ).show()
+                } else {
+                    // izin iste
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                }
+            } else {
+                // izin verilmiş galeriye git
+                val intentToGaleri =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGaleri)
+            }
+        }
+    }
+
+    private fun registerLauncher() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        secilenGorsel = intentFromResult.data
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                val source = ImageDecoder.createSource(
+                                    requireActivity().contentResolver,
+                                    secilenGorsel!!
+                                )
+                                secilenBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(secilenBitmap)
+                            } else {
+                                secilenBitmap = MediaStore.Images.Media.getBitmap(
+                                    requireActivity().contentResolver,
+                                    secilenGorsel
+                                )
+                                binding.imageView.setImageBitmap(secilenBitmap)
+                            }
+                        } catch (e: Exception) {
+                            println(e.localizedMessage)
+                        }
+
+
+                    }
+                }
+            }
+
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    //izin verildi galeriye gidebiliriz
+                    val intentToGaleri =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGaleri)
+
+                } else {
+                    Toast.makeText(requireContext(), "izin verilmedi", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     override fun onDestroyView() {
