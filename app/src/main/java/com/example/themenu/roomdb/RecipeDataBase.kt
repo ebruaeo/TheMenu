5573f48fe527ecbd3e107fc038e68f78d37f1723
ebruaeo <ebruaeo@hotmail.com>
2024-09-03 13:56:31 +0300

    database created

---
+++ app/src/main/java/com/example/themenu/roomdb/RecipeDataBase.kt
@@ -0,0 +1,10 @@
+package com.example.themenu.roomdb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.themenu.model.Recipe
+
+@Database(entities = [Recipe::class], version = 1)
+abstract class RecipeDataBase : RoomDatabase() {
+    abstract fun recipeDao(): RecipeDAO
+}
